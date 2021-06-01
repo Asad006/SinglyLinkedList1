618cf473dd8ac45f1a26be1f35613d835a864236
meroa006 <merouani.ass@gmail.com>
2021-06-01 12:03:49 -0400

    method to get the number of elements in a node list.

--- main.c
+++ main.c
@@ -6,9 +6,11 @@ typedef struct node {
     struct node *next;
 };
 
-struct node *insert_beginning(struct node *head, int item);
+struct node* insert_beginning(struct node *head, int item);
 
-struct node *insert_end(struct node *head, int item);
+struct node* insert_end(struct node *head, int item);
+
+int numberOfListElements(struct node *head);
 
 int main() {
 
@@ -16,21 +18,32 @@ int main() {
     struct node *head = NULL;
 
     int k = 1;
-    int resp;
     int element;
 
     printf("Linked list Menu!\n");
     while (k != 0) {
         printf("Option 0: Enter '0' to exit the menu and the app:\n");
         printf("Option 1: Enter '1' to insert a node at the beginning:\n");
+        printf("Option 2: Enter '2' to insert a node at the end:\n");
+        printf("Option 3: Enter '3' to get the number of nodes in the list:\n");
         scanf("%d", &k);
 
 
         switch (k) {
+            case 0:
+                break;
             case 1:
                 printf("Enter the value of the INFO:\n");
                 scanf("%d", &element);
-                insert_beginning(head, element);
+                head= insert_beginning(head, element);
+                break;
+            case 2:
+                printf("Enter the value of the INFO:\n");
+                scanf("%d", &element);
+                head=insert_end(head, element);
+                break;
+            case 3:
+                printf("The number of the elements in the list: %d\n", numberOfListElements(head));
                 break;
             default:
                 printf("Invalid entry: please enter a value from the menu list>\n");
@@ -42,8 +55,7 @@ int main() {
 
 // Pre-
 // post
-struct node *insert_beginning(struct node *head, int item) {
-    struct node *n;
+struct node* insert_beginning(struct node *head, int item) {
     struct node *temp;
     temp = (struct node *) malloc(sizeof(struct node));
     temp->info = item;
@@ -58,20 +70,39 @@ struct node *insert_beginning(struct node *head, int item) {
 
 }
 
-struct node *insert_end(struct node *head, int item) {
+struct node* insert_end(struct node *head, int item) {
     struct node *n;
     struct node *temp;
+
+    temp = (struct node *) malloc(sizeof(struct node));
+
     temp->info = item;
     temp->next = NULL;
-    temp = (struct node *) malloc(sizeof(struct node));
+
     if (head == NULL)
         head = temp;
     else {
         n = head;
         while (n->next != NULL) {
-            n=n->next;
+            n = n->next;
         }
-        n->next=temp;
+        n->next = temp;
     }
     return head;
+}
+
+int numberOfListElements(struct node *head) {
+    int k = 0;
+    struct node *temp;
+    temp = head;
+    if (temp == NULL) {
+        return 0;
+    } else {
+        while(temp!=NULL){
+            temp= temp->next;
+            k++;
+        }
+        return k;
+    }
+
 }
